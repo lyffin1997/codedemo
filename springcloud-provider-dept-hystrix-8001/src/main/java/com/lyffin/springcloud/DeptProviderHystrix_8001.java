23b73305c32a0e48963106cf96e2bfc226c96e2b
lyffin1997 <229114260@qq.com>
2022-12-18 17:12:48 +0800

    hystrix dashboard监控功能

--- springcloud-provider-dept-hystrix-8001/src/main/java/com/lyffin/springcloud/DeptProviderHystrix_8001.java
+++ springcloud-provider-dept-hystrix-8001/src/main/java/com/lyffin/springcloud/DeptProviderHystrix_8001.java
@@ -1,12 +1,15 @@
 package com.lyffin.springcloud;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
 
 @EnableEurekaClient
 @SpringBootApplication
@@ -14,5 +17,15 @@ import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 public class DeptProviderHystrix_8001 {
     public static void main(String[] args) {
         SpringApplication.run(DeptProviderHystrix_8001.class, args);
+    }
+
+    //注册bean
+    //输入localhost:8001/actuator/hystrix.stream查看是否有流信息
+    //然后在监控页面输入http://localhost:8001/actuator/hystrix.stream查看可视化监控页面
+    @Bean
+    public ServletRegistrationBean a() {
+        ServletRegistrationBean<HystrixMetricsStreamServlet> bean = new ServletRegistrationBean<>(new HystrixMetricsStreamServlet());
+        bean.addUrlMappings("/actuator/hystrix.stream");
+        return bean;
     }
 }
