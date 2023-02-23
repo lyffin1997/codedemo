367b48a0b0eb67a2020618b96f97e8f6bd64fce1
lyffin1997 <229114260@qq.com>
2023-02-23 23:42:01 +0800

    springboot整合flowable

---
+++ springboot-flowable/src/test/java/com/lyffin/flowable/SpringbootFlowableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyffin.flowable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootFlowableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
